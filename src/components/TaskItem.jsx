6ffac09255f15089803586ccc8c96feb22a4e181
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2024-01-03 18:56:51 +0530

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 const TaskItem = ({ task, onComplete, onDelete }) => {
   const { id,name, description, priority, completed } = task;
 
-  // You can use 'id' somewhere in your component, or remove it if not needed
-  // For example, you might want to use it in a link or button for editing the task
 
   return (
     <li>
